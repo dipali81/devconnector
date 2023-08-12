eb5f348f1e42a58590b6a9c25bf1bfbdf05fdca5
DipaliNeosoft <boradedipali81@gmail.com>
2023-08-12 18:03:52 +0530

    worked on post,comment,likes apis

--- config/db.js
+++ config/db.js
@@ -2,16 +2,23 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connectDB = async () =>{
-    try{
-       await mongoose.connect(db,{
-        useNewUrlParser : true
-       });
-       console.log('mongo db connected');
-    }catch(err){
-        console.error(err.message);
-        process.exit(1);
+const connectDB = async () => {
+    let retries = 5; // Number of connection attempts
+    while (retries) {
+        try {
+            await mongoose.connect(db, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+            console.log('MongoDB connected');
+            break; // Exit loop if connection is successful
+        } catch (err) {
+            console.error('MongoDB connection error:', err.message);
+            retries--;
+            console.log(`Retrying in 5 seconds... (${retries} retries left)`);
+            await new Promise(resolve => setTimeout(resolve, 5000)); // Retry after 5 seconds
+        }
     }
-}
+};
 
-module.exports =connectDB;
+module.exports = connectDB;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -237,6 +239,30 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
         console.log(err);
         res.status(500).send('server error');
     }
+});
+
+
+router.get('/github/:username',(req,res)=>{
+try{
+    const options = {
+        uri : `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+        sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+        method : 'GET',
+        headers : {'user-agent' : 'node.js'}
+    };
+    request(options,(error,response,body) => {
+        if(error) console.error(error);
+        if(response.statusCode !==200){
+            res.status(404).json({msg: 'No Github Profile Found.'})
+        }
+        res.json(JSON.parse(body));
+    });
+
+}catch(err){
+    console.log(err);
+    res.status(500).send('server error');
+    
+}
 });
 
 module.exports = router;
